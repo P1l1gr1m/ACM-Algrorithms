5e957115d3d826a2df26fe0f69aae6fcee85d929
P1l1gr1m <91072080+P1l1gr1m@users.noreply.github.com>
2021-10-18 22:45:16 +0300

    Add files via upload

---
+++ Matrix/Matrix/Matrix.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int number, s;
+	cin >> s;
+	cin >> number;
+	vector<int> volume;
+	volume.push_back(number);
+	int help = 0;
+	while (!cin.eof()) {
+		cin >> number;
+		if (help % 2 == 0) {
+			volume.push_back(number);
+		}
+		++help;
+	}
+	long long int** matrix = new long long int* [s + 1];
+	for (int i = 0; i < s + 1; ++i) {
+		matrix[i] = new long long int[s + 1];
+	}
+	for (int i = 1; i <= s; ++i) {
+		matrix[i][i] = 0;
+	}
+	for (int n = 2; n <= s; ++n) {
+		for (int i = 1; i <= s + 1 - n; ++i) {
+			int j = i + n - 1;
+			matrix[i][j] = LONG_MAX;
+			for (int l = i; l <= j - 1; ++l) {
+				matrix[i][j] = min(matrix[i][j], matrix[i][l] + matrix[l + 1][j] + volume[i - 1] * volume[j]);
+			}
+		}
+	}
+	cout << matrix[1][s];
+}
---
+++ Palindrome/Palindrome/Palindrome.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void getPalindrome(string str, int size) {
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
+	int** mas = new int* [size];
+	for (int i = 0; i < size; i++) {
+		mas[i] = new int[size];
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			mas[i][j] = 0;
+		}
+		mas[i][i] = 1;
+	}
+	for (int q = 1; q < size; q++) {
+		for (int i = 0; i < size - q; i++) {
+			int j = i + q;
+			if (str[i] == str[j]) {
+				mas[i][j] = mas[i + 1][j - 1] + 2;
+			}
+			else {
+				if (mas[i + 1][j] > mas[i][j - 1]) mas[i][j] = mas[i + 1][j];
+				else mas[i][j] = mas[i][j - 1];
+			}
+		}
+	}
+	int l = 0, r = size - 1;
+	string result;
+	int palR = mas[0][size - 1] - 1, palL = 0;
+	result.resize(palR + 1);
+	while (l <= r) {
+		if (l == r && mas[l][r] == 1) result[palL++] = str[l++];
+		else {
+			if (str[l] == str[r]) {
+				result[palL++] = str[l++];
+				result[palR--] = str[r--];
+			}
+			else {
+				if (mas[l + 1][r] >= mas[l][r - 1]) l++;
+				else r--;
+			}
+		}
+	}
+
+	fout << mas[0][size - 1] << endl;
+	fout << result;
+}
+
+int main() {
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
+
+	string word;
+	fin >> word;
+	getPalindrome(word, word.size());
+	return 0;
+}
